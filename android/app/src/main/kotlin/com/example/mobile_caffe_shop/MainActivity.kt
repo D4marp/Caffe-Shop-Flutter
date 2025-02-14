2a42c6eb19556361792eaf21e626fd52a21b5027
D4marp <damargalihajipradana@it.student.pens.ac.id>
2025-02-14 15:10:24 +0700

    new Project

---
+++ android/app/src/main/kotlin/com/example/mobile_caffe_shop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mobile_caffe_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/widgets/navigation_bar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NavigationBar extends StatelessWidget {
+  const NavigationBar ({super.key, required this.title, required this.onPressed});
+  final String title;
+  final VoidCallback onPressed;
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
